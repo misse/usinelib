33ff7c1a80987ab7548e1003b7b12d9b8d601b40
Martin Bergman <martin@devsed.se>
2020-05-22 13:35:41 +0200

    update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="usinelib-misse",
+    name="usinelib",
     version="0.0.1",
     author="Martin Bergman",
     author_email="martin@devsed.se",

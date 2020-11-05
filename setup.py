bdf40fca3fe0efe440086facf1ebe554591703e6
Martin Bergman <martin.bergman@dice.se>
2020-11-05 15:49:19 +0100

    update setup to support all python3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="usinelib",
-    version="0.0.1",
+    version="0.0.4",
     author="Martin Bergman",
     author_email="martin@devsed.se",
     description="Fetch menu from usine.se",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
 )
